20f7c5f94400a32a10054f1ac45c3d244b3484b9
ElectroSpeed <150343183+ElectroSpeed@users.noreply.github.com>
2024-02-02 13:53:27 +0100

    Menu
    EndMenu
    PauseMenu
    Add an input Menu Active/DesactiveEndMenu
    Modification MenuManager

--- DuoParty/Assets/Scripts/ButtonManager.cs
+++ DuoParty/Assets/Scripts/ButtonManager.cs
@@ -8,8 +8,11 @@ using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
-    [SerializeField] private GameObject _mainMenu;
+    [SerializeField] private GameObject _menu, _endMenu;
     public AudioSource _audioSource;
+    private bool _pauseActive;
+    private bool _endActive;
+
     private void Start()
     {
         _audioSource.Play();
@@ -20,9 +23,35 @@ public class ButtonManager : MonoBehaviour
         SceneManager.LoadScene("Game");
         Time.timeScale = 1f;
     }
-    public void Mainmenu()
+    public void MainMenu()
     {
         SceneManager.LoadScene("MainMenu");
+    }
+    public void PauseMenu()
+    {
+        if (!_pauseActive) 
+        {
+            GameObject.Find("MenuManager").transform.Find("PauseMenu").gameObject.SetActive(true);
+            _pauseActive = true;
+        }
+        else
+        {
+            GameObject.Find("MenuManager").transform.Find("PauseMenu").gameObject.SetActive(false);
+            _pauseActive = false;
+        }
+    }
+    public void EndMenu()
+    {
+        if (!_pauseActive)
+        {
+            GameObject.Find("MenuManager").transform.Find("EndMenu").gameObject.SetActive(true);
+            _pauseActive = true;
+        }
+        else
+        {
+            GameObject.Find("MenuManager").transform.Find("EndMenu").gameObject.SetActive(false);
+            _pauseActive = false;
+        }
     }
     public void QuitGame()
     {
