f4a2bcd874cab4d7a3e4ab79e627b526b84807c1
ElectroSpeed <150343183+ElectroSpeed@users.noreply.github.com>
2024-02-05 13:49:21 +0100

    Checkmap
    Check element in the map and _centerCase list, _borderCase list

--- DuoParty/Assets/Scripts/BoardGame/Case.cs
+++ DuoParty/Assets/Scripts/BoardGame/Case.cs
@@ -1,10 +1,19 @@
+using System;
 using UnityEngine;
 
 public class Case : MonoBehaviour
 {
+    [SerializeField] string _coordinatesLength;
+    [SerializeField] string _coordinatesWidth;
+
     [SerializeField] bool isInteractible = true;
     [SerializeField] private bool isSpawn;
     [SerializeField] private bool isEnd;
+    [SerializeField] private bool isKey;
+    [SerializeField] private bool isAccessCard;
+    [SerializeField] private bool isHammer;
+    [SerializeField] private bool isBomb;
+    [SerializeField] private bool isArmouredDoor;
 
     [SerializeField] private string color;
 
@@ -27,5 +36,35 @@ public class Case : MonoBehaviour
     {
         return color;
     }
-    
+
+    public bool GetKey()
+    {
+        return isKey;
+    }
+    public bool GetAccessCard()
+    {
+        return isAccessCard;
+    }
+    public bool GetHammer()
+    {
+        return isHammer;
+    }
+    public bool GetBomb()
+    {
+        return isBomb;
+    }
+    public bool GetArmouredDoor()
+    {
+        return isArmouredDoor;
+    }
+
+    public string GetCoordinatesLength()
+    {
+        return _coordinatesLength;
+    }
+
+    public string GetCoordinatesWidth()
+    {
+        return _coordinatesWidth;
+    }
 }
--- DuoParty/Assets/Scripts/BoardGame/GridManager.cs
+++ DuoParty/Assets/Scripts/BoardGame/GridManager.cs
@@ -7,6 +7,9 @@ public class GridManager : MonoBehaviour
     [SerializeField] private GameObject redPlayer;
     [SerializeField] private GameObject greenPlayer;
 
+    public List<Case> _borderCases;
+    public List<Case> _centerCases;
+
     private void Start()
     {
         foreach(Case _case in cases)
@@ -20,6 +23,43 @@ public class GridManager : MonoBehaviour
             {
                 //Instantiate(greenPlayer, _case.transform.position, Quaternion.identity);
                 greenPlayer.transform.position = _case.transform.position;
+            }
+            if (_case.GetKey())
+            {
+                Debug.Log("Key");
+            }
+            if (_case.GetAccessCard())
+            {
+                Debug.Log("AccessCard");
+            }
+            if (_case.GetHammer())
+            {
+                Debug.Log("Hammer");
+            }
+            if (_case.GetBomb())
+            {
+                Debug.Log("Bomb");
+            }
+            if (_case.GetArmouredDoor())
+            {
+                Debug.Log("ArmouredDoor");
+            }
+        }
+    }
+
+
+    public void SetBorderList(List<GameObject> list)
+    {
+        foreach (Case _case in cases)
+        {
+            if (_case.GetCoordinatesLength() == "A" || _case.GetCoordinatesLength() == "I"
+                || _case.GetCoordinatesWidth() == "1" || _case.GetCoordinatesWidth() == "9")
+            {
+                _borderCases.Add(_case);
+            }
+            else
+            {
+                _centerCases.Add(_case);
             }
         }
     }
